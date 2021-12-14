7101f1f67b3f9527ce311a1ed7e3a50ec10dda8e
LumRamabaja <lumramabaja@gmail.com>
2021-12-14 22:39:55 +0100

    added boost test

--- tests/LiquidNFT_test.py
+++ tests/LiquidNFT_test.py
@@ -38,7 +38,7 @@ def test_get_new_underlying_value(liquid_nft, deep_pool, token):
     underlying_value = liquid_nft.getUnderlyingValue(1)
     assert underlying_value == 30e18
 
-def test_get_underlying_value_after_new_mint(liquid_nft, deep_pool, token):
+def test_get_underlying_value_after_new_mint(liquid_nft, token):
     token.mint(accounts[1], 1000e18, {'from': accounts[0]})
     token.approve(liquid_nft.address, 10e18, {'from': accounts[1]})
     liquid_nft.mint(accounts[1], 10e18, {'from': accounts[1]})
@@ -50,3 +50,19 @@ def test_get_underlying_value_after_new_mint(liquid_nft, deep_pool, token):
     underlying_value2 = liquid_nft.getUnderlyingValue(2)
     assert underlying_value1 == liquidity1 + (20e18 * share1 / (share1 + share2))
     assert underlying_value2 == liquidity2 + (20e18 * share2 / (share1 + share2))
+
+def test_boost(liquid_nft, token):
+    token.approve(liquid_nft.address, 50e18, {'from': accounts[1]})
+    liquid_nft.boost(50e18, 2, {'from': accounts[1]})
+    liquidity1, share1, _, _ = liquid_nft.getNFTData(1) 
+    liquidity2, share2, _, _ = liquid_nft.getNFTData(2) 
+    assert share1 == 1e18 
+    new_share = (1e18 * 10e18 / (2*20e18)) + (1e18 * 50e18 / (3 * 70e18))
+    # python rounding error
+    assert (share2 - new_share) == -5
+    underlying_value1 = liquid_nft.getUnderlyingValue(1)
+    dif = underlying_value1 - (liquidity1 + (20e18 * share1 / (share1 + share2)))
+    assert dif == 2002
+    underlying_value2 = liquid_nft.getUnderlyingValue(2)
+    dif = underlying_value2 - (liquidity2 + (20e18 * share2 / (share1 + share2)))
+    assert dif == 997
