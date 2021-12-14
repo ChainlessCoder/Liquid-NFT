eba7e47b4acad1625a777e964bc5979053a17554
LumRamabaja <lumramabaja@gmail.com>
2021-12-14 19:15:26 +0100

    modified mock

--- contracts/mocks/FeeGeneratingContract.sol
+++ contracts/mocks/FeeGeneratingContract.sol
@@ -10,23 +10,27 @@ import "../../interfaces/IMintableERC20Mock.sol";
 contract FeeGeneratingContract is IFeeGeneratingContract {
     using SafeERC20 for IERC20;
 
-    address private immutable liquidNFT;
+    address private liquidNFT;
     address private erc20Token;
 
     constructor(
-        address _liquidNFT,
         address _erc20Token
     ) {
-        liquidNFT = _liquidNFT;
         erc20Token = _erc20Token;
     }
 
     function getFeeReserve() external view override returns (uint256 reserve) {
         reserve = IERC20(erc20Token).balanceOf(address(this));
     }
+
     function redeemFees(address _to, uint256 _shareAmount) external override returns (bool) {
         require(msg.sender == liquidNFT, "Mock: msg.sender must be Liquid NFT contract");
         IERC20(erc20Token).safeTransfer(_to, _shareAmount);
         return true;
     }
+
+    function setLiquidNFTAddress(address _liquidNFT) external override returns (bool) {
+        liquidNFT = _liquidNFT;
+    }
+
 }
--- interfaces/IFeeGeneratingContract.sol
+++ interfaces/IFeeGeneratingContract.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.0;
 interface IFeeGeneratingContract {
     function getFeeReserve() external view returns (uint256);
     function redeemFees(address _to, uint256 _shareAmount) external returns (bool);
+    function setLiquidNFTAddress(address _liquidNFT) external returns (bool);
 }
