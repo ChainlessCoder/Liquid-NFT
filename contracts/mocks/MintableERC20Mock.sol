0d3be0daec3c707172fc57fd44ad896b14f26377
LumRamabaja <lumramabaja@gmail.com>
2021-12-14 19:33:49 +0100

    added deployment mocks

--- contracts/mocks/MintableERC20Mock.sol
+++ contracts/mocks/MintableERC20Mock.sol
@@ -7,16 +7,7 @@ import "../../interfaces/IMintableERC20Mock.sol";
 
 
 contract MintableERC20Mock is IMintableERC20Mock, ERC20 {
-    address private immutable liquidNFT;
-    address private erc20Token;
-
-    constructor(
-        address _liquidNFT,
-        address _erc20Token
-    ) ERC20("MockToken", "MOCK") {
-        liquidNFT = _liquidNFT;
-        erc20Token = _erc20Token;
-    }
+    constructor() ERC20("MockToken", "MOCK") {}
 
     function mint(address _to, uint256 _amount) external override returns (bool) {
         _mint(_to, _amount);
